872c87bcceabf2e30cebc02c9cdd9215f6e884c1
ceofficial <40243447+ceofficial@users.noreply.github.com>
2019-01-24 18:44:09 +0600

    fix

--- flappy/js/game.js
+++ flappy/js/game.js
@@ -7,11 +7,11 @@ var fg = new Image();
 var pipeUp = new Image();
 var pipeBottom = new Image();
 
-bird.src = "img/admin.png";
-bg.src = "img/bg.png";
-fg.src = "img/fg.png";
-pipeUp.src = "img/pipeUp.png";
-pipeBottom.src = "img/pipeBottom.png";
+bird.src = "img/admin1.png";
+bg.src = "img/bg1.png";
+fg.src = "img/fg1.png";
+pipeUp.src = "img/2.png";
+pipeBottom.src = "img/1.png";
 
 // Звуковые файлы
 var fly = new Audio();
@@ -20,14 +20,13 @@ var score_audio = new Audio();
 fly.src = "audio/fly.mp3";
 score_audio.src = "audio/score.mp3";
 
-var gap = 90;
+var gap = 200;
 
-// При нажатии на какую-либо кнопку
-document.addEventListener("keydown", moveUp);
+// При нажатии на экран
 document.addEventListener("click", moveUp);
 
 function moveUp() {
- yPos -= 25;
+ yPos -= 45; //высота полета при нажатии на экран
  fly.play();
 }
 
@@ -43,7 +42,7 @@ var score = 0;
 // Позиция птички
 var xPos = 10;
 var yPos = 150;
-var grav = 1.5;
+var grav = 1.7;
 
 function draw() {
  ctx.drawImage(bg, 0, 0);
@@ -66,7 +65,12 @@ function draw() {
  && xPos <= pipe[i].x + pipeUp.width
  && (yPos <= pipe[i].y + pipeUp.height
  || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height) {
- location.reload(); // Перезагрузка страницы
+ //location.reload(); // Перезагрузка страницы
+ //window.location.reload();
+//window.location.href = window.location.href; //This is a possibility
+//window.location.reload(true); //Another possiblity
+//history.go(0); //And another
+window.location = document.URL
  }
 
  if(pipe[i].x == 5) {
